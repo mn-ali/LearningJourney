750825afc8fd9e2e5257f3a6e81dbd3a2719ecc9
mn-ali <nusrathali207@gmail.com>
2024-09-26 22:45:15 +0530

    Create oddeven.py

---
+++ oddeven.py
@@ -0,0 +1,7 @@
+# Odd/Even:
+
+a = int(input("Enter a Number: "))
+if(a%2==0):  # If Remainder = 0 on division by 2, it's obviosly even!
+    print("The number is even!")
+else:
+    print("The number is odd!")
