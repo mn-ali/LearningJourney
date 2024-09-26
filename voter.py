cd99e0ddf784badbc032ff57694a52396a37ff78
mn-ali <nusrathali207@gmail.com>
2024-09-26 22:45:47 +0530

    Create voter.py

---
+++ voter.py
@@ -0,0 +1,9 @@
+a = int(input("Enter Your Age: "))
+if(a>=0 and a<18):
+    print("You're not eligible for Voting Nigga!")
+elif(a<0):
+    print("Nigga why is your age Negative?")
+elif(a>80):
+    print("Nigga RIP!")
+else:
+    print("You're eligible for Voting!")
