5fc2af98a8cf9be26c33d4a7cee74a3b03bc24c4
mn-ali <nusrathali207@gmail.com>
2024-09-26 22:47:47 +0530

    Create ABCDgrade.py

---
+++ ABCDgrade.py
@@ -0,0 +1,14 @@
+a = int(input("Enter Your Marks: "))
+if a>100:
+    print("Nigga you lyin")
+if a<=100 and a>90:
+    print("A - Grade!")
+if a>80 and a<=90:
+    print("B - Grade")
+if a>=60 and a<=80:
+    print("C - Grade")
+if a<60:
+    print("D - Grade")
+if a<0:
+    print("Nigga >:-(")
+
